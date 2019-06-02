28b4faa004176e96e880b7c8c82cf98a498e3657
dtirado <dtirado80@msn.com>
2019-06-02 00:22:02 -0500

    Fine tuning and missing files

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+
+var resTable=[
+    {
+        "name":"Daniel",
+        "photo":"https://media.licdn.com/dms/image/C5603AQHJUwEwcv8HrQ/profile-displayphoto-shrink_200_200/0?e=1565222400&v=beta&t=W26T3kYfPLDUYRTNv1yv19TrvQOoG80-QXXWFFn6ERk",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+      
+];
+
+module.exports=resTable;
